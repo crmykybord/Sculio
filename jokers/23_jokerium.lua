65dbf718346fc69a6f4aa93ada7d978938d684e7
Marffe <efframlol@gmail.com>
2026-05-29 19:27:31 -0500

    Jokerium: Levels up all hands instead of creating a blackhole

--- jokers/23_jokerium.lua
+++ jokers/23_jokerium.lua
@@ -1,7 +1,7 @@
 SMODS.Joker {
   key = 'jokerium',
 
-  config = { extra = { black_holes_per_cycle = 1, boss_blinds_per_cycle = 1, boss_blinds_since_last_cycle = 0 } },
+  config = { extra = { levels = 1 } },
   unlocked = true,
   discovered = false,
   rarity = 2, -- Uncommon
@@ -10,40 +10,23 @@ SMODS.Joker {
   cost = 7,
   blueprint_compat = true,
   loc_vars = function(self, info_queue, card)
-    info_queue[#info_queue+1] = G.P_CENTERS.e_negative
-    info_queue[#info_queue+1] = G.P_CENTERS.c_black_hole
-    return { vars = { card.ability.extra.black_holes_per_cycle, card.ability.extra.boss_blinds_per_cycle, card.ability.extra.boss_blinds_per_cycle - card.ability.extra.boss_blinds_since_last_cycle } }
+    return { vars = { card.ability.extra.levels } }
   end,
   calculate = function(self, card, context)
     if context.end_of_round and not context.repetition and context.game_over == false and G.GAME.blind.boss then
-      card.ability.extra.boss_blinds_since_last_cycle = card.ability.extra.boss_blinds_since_last_cycle + 1
-
-      if card.ability.extra.boss_blinds_since_last_cycle >= card.ability.extra.boss_blinds_per_cycle then
-        card.ability.extra.boss_blinds_since_last_cycle = 0
-
-        for i = 1, card.ability.extra.black_holes_per_cycle, 1 do
-          G.E_MANAGER:add_event(Event({
-            delay = 0.0,
-            func = function()
-              local card = create_card('Spectral', G.consumeables, nil, nil, nil, nil, 'c_black_hole')
-              card:set_edition({negative = true}, true)
-              card:add_to_deck()
-              G.consumeables:emplace(card)
-              return true
-            end
-          }))
+      for hand, hand_data in pairs(G.GAME.hands) do
+        if hand_data.visible then
+          for i = 1, card.ability.extra.levels, 1 do
+            update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=localize(hand, 'poker_hands'),chips = hand_data.chips, mult = hand_data.mult, level=hand_data.level})
+            level_up_hand(context.blueprint_card or card, hand, nil, 1)
+          end
         end
-
-        return {
-            message = '+' .. card.ability.extra.black_holes_per_cycle .. ' Black Hole',
-            colour = G.C.SECONDARY_SET.Spectral
-        }
-      else
-        return {
-          message = card.ability.extra.boss_blinds_since_last_cycle .. '/' .. card.ability.extra.boss_blinds_per_cycle,
-          colour = G.C.FILTER
-        }
       end
+
+      return {
+        message = localize('k_upgrade_ex'),
+        colour = G.C.FILTER
+      }
     end
   end
 }
