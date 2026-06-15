a465f4d6dfc4931ccebe569ba6f06f444b7afe03
Marffe <efframlol@gmail.com>
2026-06-14 22:44:25 -0500

    Only debuffs once instead of every round

--- jokers/37_pharaoh.lua
+++ jokers/37_pharaoh.lua
@@ -11,17 +11,33 @@ SMODS.Joker {
   loc_vars = function(self, info_queue, card)
     return { vars = { card.ability.extra.x_mult_bonus } }
   end,
+  add_to_deck = function(self, card, from_debuff)
+    if G.hand then
+      for _, v in ipairs(G.hand.cards) do
+        if not v:is_face() then v:set_debuff(true) end
+      end
+    end
+  end,
+  remove_from_deck = function(self, card, from_debuff)
+    if G.hand then
+      for _, v in ipairs(G.hand.cards) do
+        v:set_debuff(false)
+      end
+    end
+  end,
   calculate = function(self, card, context)
-    for k, v in ipairs(G.hand.cards) do
-      if not v:is_face() then
-        v:set_debuff(true)
+    if context.before then
+      for _, v in ipairs(G.hand.cards) do
+        if not v:is_face() then
+          v:set_debuff(true)
+        end
       end
     end
 
     if context.individual and context.cardarea == G.play then
       if not context.other_card.debuff and context.other_card:is_face() then
         return {
-          x_mult = card.ability.extra.x_mult_bonus,
+          xmult = card.ability.extra.x_mult_bonus,
           card = card
         }
       end
