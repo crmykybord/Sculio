2575bed7a0709f17ed31e9ba851dee9c24361ad3
Marffe <efframlol@gmail.com>
2026-06-17 01:02:01 -0500

    This took too much to fix

--- jokers/32_sensory_overload.lua
+++ jokers/32_sensory_overload.lua
@@ -9,11 +9,16 @@ SMODS.Joker {
   atlas = 'Sculio',
   pos = { x = 3, y = 3 },
   cost = 6,
+  blueprint_compat = true,
   loc_vars = function(self, info_queue, card)
     return { vars = { card.ability.extra.money_gain, card.ability.extra.triggers_per_gain, card.ability.extra.triggers_since_gain } }
   end,
   calculate = function(self, card, context)
-    if context.post_trigger and not (context.other_card.config and context.other_card.config.center and context.other_card.config.center.key == 'j_Sculio_sensory_overload') then
+    if context.post_trigger then
+      local trigger_card = context.other_card
+
+      -- Don't count triggers caused by Sensory Overload itself (avoid infinite loop)
+      if not trigger_card or trigger_card == card then return end
       card.ability.extra.triggers_since_gain = card.ability.extra.triggers_since_gain + 1
 
       if card.ability.extra.triggers_since_gain >= card.ability.extra.triggers_per_gain then
