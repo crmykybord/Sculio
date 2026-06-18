d0b0acdd2618aa79738fe1c7e959f95252178926
Marffe <efframlol@gmail.com>
2026-06-18 17:22:14 -0500

    I forgot Figurine xd

--- jokers/14_figurine.lua
+++ jokers/14_figurine.lua
@@ -16,36 +16,32 @@ SMODS.Joker {
   end,
   calculate = function(self, card, context)
     if context.cardarea == G.jokers and context.selling_card and context.card ~= card and not context.blueprint then
-      local sold_joker = context.card
-      local message = nil
-      local gain
-
-      if not sold_joker.debuff and sold_joker.edition then
-        if sold_joker.edition.type == 'foil' then
-          gain = 50 * card.ability.extra.bonus_mult
-          message = localize { type = 'variable', key = 'a_chips', vars = { gain } }
-          card.ability.extra.chips = card.ability.extra.chips + gain
-        end
-
-        if sold_joker.edition.type == 'holo' then
-          gain = 10 * card.ability.extra.bonus_mult
-          message = localize { type = 'variable', key = 'a_mult', vars = { gain } }
-          card.ability.extra.mult = card.ability.extra.mult + gain
-        end
-
-        if sold_joker.edition.type == 'polychrome' then
-          gain = 0.5 * card.ability.extra.bonus_mult
-          message = '+ ' .. localize { type = 'variable', key = 'a_xmult', vars = { gain } }
-          card.ability.extra.x_mult = card.ability.extra.x_mult + gain
-        end
-
-        if sold_joker.edition.type == 'fluorite' then
-          gain = 0.5 * card.ability.extra.bonus_mult
-          message = '+ ' .. localize { type = 'variable', key = 'a_xchips', vars = { gain } }
-          card.ability.extra.x_chips = card.ability.extra.x_chips + gain
-        end
+      local sold = context.card
+      if sold.debuff or not sold.edition then return end
+      local ed_key = sold.edition.type or sold.edition.key
+      local ed_center = G.P_CENTERS[ed_key] or G.P_CENTERS['e_'..ed_key]
+      if not ed_center then return end
+      local cfg = ed_center.config
+      local bonus = card.ability.extra.bonus_mult
+      local message, gain
+      if cfg.chips and cfg.chips > 0 then
+        gain = cfg.chips * bonus
+        card.ability.extra.chips = card.ability.extra.chips + gain
+        message = localize({ type = 'variable', key = 'a_chips', vars = { gain } })
+      elseif cfg.mult and cfg.mult > 0 then
+        gain = cfg.mult * bonus
+        card.ability.extra.mult = card.ability.extra.mult + gain
+        message = localize({ type = 'variable', key = 'a_mult', vars = { gain } })
+      elseif cfg.x_mult and cfg.x_mult > 1 then
+        gain = (cfg.x_mult - 1) * bonus
+        card.ability.extra.x_mult = card.ability.extra.x_mult + gain
+        message = '+ ' .. localize({ type = 'variable', key = 'a_xmult', vars = { gain } })
+      elseif (cfg.x_chips and cfg.x_chips > 1) or (cfg.Xchips and cfg.Xchips > 1) then
+        local xchips_val = cfg.x_chips or cfg.Xchips
+        gain = (xchips_val - 1) * bonus
+        card.ability.extra.x_chips = card.ability.extra.x_chips + gain
+        message = '+ ' .. localize({ type = 'variable', key = 'a_xchips', vars = { gain } })
       end
-
       if message then
         G.E_MANAGER:add_event(Event({
           func = function()
@@ -55,9 +51,8 @@ SMODS.Joker {
         }))
       end
     end
-
     if context.joker_main then
-      return { chips = card.ability.extra.chips, mult = card.ability.extra.mult, xmult = card.ability.extra.x_mult, xchips = card.ability.extra.x_chips }
+      return { chips = card.ability.extra.chips, mult = card.ability.extra.mult, xmult = card.ability.extra.x_mult, x_chips = card.ability.extra.x_chips }
     end
   end
 }
