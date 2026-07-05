27dee73635bba446beb201228f43bf065ff52a90
Marffe <efframlol@gmail.com>
2026-07-05 16:49:32 -0500

    Fixed sprite display of the Unstopable tag

--- Sculio.lua
+++ Sculio.lua
@@ -1,6 +1,6 @@
 Sculio = SMODS.current_mod
 SMODS.Atlas { key = 'Sculio', path = 'Sculio.png', px = 71, py = 95 }
-SMODS.Atlas { key = 'Sculio_Tags', path = 'Tags.png', px = 34, py = 3 }
+SMODS.Atlas { key = 'Sculio_Tags', path = 'Tags.png', px = 34, py = 34 }
 
 SMODS.current_mod.optional_features = function()
   return { post_trigger = true, object_weights = true }
