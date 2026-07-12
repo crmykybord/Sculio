a836c9c1d4487fcffcaac7f2eef67251ce4c4e7f
Marffe <efframlol@gmail.com>
2026-07-12 02:37:12 -0500

    Name change for files

--- jokers/50_jokes_against_humanity.lua
+++ jokers/50_jokes_against_humanity.lua
@@ -1,5 +1,5 @@
 SMODS.Joker {
-  key = 'card_against',
+  key = 'jokes_against_humanity',
   attributes = { 'xmult', "chance", "joker" },
   eternal_compat = true,
   blueprint_compat = true,
@@ -8,7 +8,7 @@ SMODS.Joker {
   config = { extra = { x_mult = 4, debuffed_jokers = {} } },
   unlocked = true,
   discovered = false,
-  rarity = 3, -- Rare
+  rarity = 3,
   atlas = 'Sculio',
   pos = { x = 2, y = 5 },
   cost = 10,
@@ -29,8 +29,8 @@ SMODS.Joker {
 
       local available_jokers = {}
       for _, j in ipairs(G.jokers.cards) do
-        local is_card_against = j.config and j.config.center and j.config.center.key == 'j_Sculio_card_against'
-        if j ~= card and not is_card_against then
+        local is_jokes_against_humanity = j.config and j.config.center and j.config.center.key == 'j_Sculio_jokes_against_humanity'
+        if j ~= card and not is_jokes_against_humanity then
           table.insert(available_jokers, j)
         end
       end
@@ -41,13 +41,13 @@ SMODS.Joker {
           table.insert(shuffled, j)
         end
         for i = #shuffled, 2, -1 do
-          local j_idx = pseudorandom('card_against_shuffle', 1, i)
+          local j_idx = pseudorandom('jokes_against_humanity_shuffle', 1, i)
           shuffled[i], shuffled[j_idx] = shuffled[j_idx], shuffled[i]
         end
 
         local to_debuff = math.min(2, #shuffled)
         for i = 1, to_debuff do
-          if SMODS.pseudorandom_probability(card, 'card_against', 1, 4) then
+          if SMODS.pseudorandom_probability(card, 'jokes_against_humanity', 1, 4) then
             shuffled[i]:set_debuff(true)
             table.insert(card.ability.extra.debuffed_jokers, shuffled[i])
           end
--- jokers/51_letter_tile.lua
+++ jokers/51_letter_tile.lua
@@ -1,5 +1,5 @@
 SMODS.Joker {
-  key = 'scrabble',
+  key = 'letter_tile',
   attributes = { 'mult', 'jack' },
   eternal_compat = true,
   blueprint_compat = true,
@@ -8,16 +8,16 @@ SMODS.Joker {
   config = { extra = { mult = 8 } },
   unlocked = true,
   discovered = false,
-  rarity = 2, -- Uncommon
+  rarity = 2,
   atlas = 'Sculio',
   pos = { x = 3, y = 5 },
-  cost = 6,
+  cost = 5,
   loc_vars = function(self, info_queue, card)
     return { vars = { card.ability.extra.mult } }
   end,
   calculate = function(self, card, context)
     if context.individual and context.cardarea == G.play then
-      if context.other_card:get_id() == 11 then -- Jack
+      if context.other_card:get_id() == 11 then
         return { mult = card.ability.extra.mult }
       end
     end
