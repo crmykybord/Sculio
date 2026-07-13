9fa296b2fe0a98596fffc9492cc722a9b63d7d25
Marffe <efframlol@gmail.com>
2026-07-12 19:26:33 -0500

    object_weight was not being used nowhere

--- Sculio.lua
+++ Sculio.lua
@@ -3,7 +3,7 @@ SMODS.Atlas { key = 'Sculio', path = 'Sculio.png', px = 71, py = 95 }
 SMODS.Atlas { key = 'Sculio_Tags', path = 'Tags.png', px = 34, py = 34 }
 
 SMODS.current_mod.optional_features = function()
-  return { post_trigger = true, object_weights = true }
+  return { post_trigger = true }
 end
 
 -- Talisman compat
