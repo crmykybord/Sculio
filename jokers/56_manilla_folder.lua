dcce2d732f3d0669610418a7b5370586d254775c
Marffe <efframlol@gmail.com>
2026-07-14 23:03:49 -0500

    Manilla Joker working as intended

--- jokers/56_manilla_folder.lua
+++ jokers/56_manilla_folder.lua
@@ -1,3 +1,25 @@
+-- The 10 standard Balatro poker hands. Anything else (vanilla secret hands like
+-- Five of a Kind / Flush Five / Flush House, or modded secret hands registered
+-- via SMODS.PokerHand) is treated as a secret hand and triggers Manilla Folder.
+local STANDARD_HANDS = {
+  ['High Card'] = true,
+  ['Pair'] = true,
+  ['Two Pair'] = true,
+  ['Three of a Kind'] = true,
+  ['Straight'] = true,
+  ['Flush'] = true,
+  ['Full House'] = true,
+  ['Four of a Kind'] = true,
+  ['Straight Flush'] = true,
+  ['Royal Flush'] = true,
+}
+
+local function is_secret_hand(hand_name)
+  if not hand_name or STANDARD_HANDS[hand_name] then return false end
+  -- The hand must be registered in G.GAME.hands (covers both vanilla and SMODS-registered hands).
+  return G.GAME and G.GAME.hands and G.GAME.hands[hand_name] ~= nil
+end
+
 SMODS.Joker {
   key = 'manilla_folder',
   attributes = { 'consumables', 'secret_hand' },
@@ -16,46 +38,40 @@ SMODS.Joker {
     return { vars = {} }
   end,
   calculate = function(self, card, context)
-    if context.evaluate_poker_hand and not context.blueprint then
-      local hand_name = context.scoring_name
-      local secret_hands = {
-        ['Five of a Kind'] = true,
-        ['Flush Five'] = true,
-        ['Flush House'] = true
-      }
-      if secret_hands[hand_name] then
-        G.E_MANAGER:add_event(Event({
-          func = function()
-            for i = 1, #G.consumeables.cards do
-              G.consumeables.cards[i]:remove()
-            end
-            G.consumeables.cards = {}
-            local num_slots = G.consumeables.config.card_limit or 3
-            for i = 1, num_slots do
-              if pseudorandom('manilla_folder_type') < 0.9 then
-                local consume_type = pseudorandom('manilla_folder_consumable')
-                local new_consumable
-                if consume_type < 0.4 then
-                  new_consumable = SMODS.create_card({ set = 'Tarot', rarity = 1, no_soul = true })
-                elseif consume_type < 0.7 then
-                  new_consumable = SMODS.create_card({ set = 'Planet', rarity = 1, no_soul = true })
-                else
-                  new_consumable = SMODS.create_card({ set = 'Spectral', rarity = 1, no_soul = true })
-                end
-                if new_consumable then
-                  G.consumeables:emplace(new_consumable)
-                end
+    -- Trigger BEFORE the hand is played (when the player commits to a Secret Hand).
+    -- This matches the description: "When playing a Secret Hand, fill empty consumable slots".
+    if context.before and context.scoring_name and not context.blueprint and is_secret_hand(context.scoring_name) then
+      G.E_MANAGER:add_event(Event({
+        blockable = true,
+        func = function()
+          if not G.consumeables or not G.consumeables.cards or not G.consumeables.config then return true end
+          local num_slots = G.consumeables.config.card_limit or 2
+          local empty_slots = num_slots - #G.consumeables.cards
+          if empty_slots <= 0 then return true end
+          for _ = 1, empty_slots do
+            if pseudorandom('manilla_folder_type') < 0.9 then
+              local consume_type = pseudorandom('manilla_folder_consumable')
+              local new_consumable
+              if consume_type < 0.4 then
+                new_consumable = SMODS.create_card({ set = 'Tarot', rarity = 1, no_soul = true })
+              elseif consume_type < 0.7 then
+                new_consumable = SMODS.create_card({ set = 'Planet', rarity = 1, no_soul = true })
               else
-                local new_consumable = SMODS.create_card({ set = 'Spectral', rarity = 1, no_soul = true })
-                if new_consumable then
-                  G.consumeables:emplace(new_consumable)
-                end
+                new_consumable = SMODS.create_card({ set = 'Spectral', rarity = 1, no_soul = true })
+              end
+              if new_consumable and G.consumeables then
+                G.consumeables:emplace(new_consumable)
+              end
+            else
+              local new_consumable = SMODS.create_card({ set = 'Spectral', rarity = 1, no_soul = true })
+              if new_consumable and G.consumeables then
+                G.consumeables:emplace(new_consumable)
               end
             end
-            return true
           end
-        }))
-      end
+          return true
+        end
+      }))
     end
   end
 }
