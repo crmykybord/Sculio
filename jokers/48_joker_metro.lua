4050c8f97eeb9fd60b60f9f73c9aa84c5943b23f
Marffe <efframlol@gmail.com>
2026-07-14 23:10:48 -0500

    Joker Metro rework is working as expected

--- jokers/48_joker_metro.lua
+++ jokers/48_joker_metro.lua
@@ -1,4 +1,10 @@
-local hand_order = { 'High Card', 'Pair', 'Two Pair', 'Three of a Kind', 'Straight', 'Flush', 'Four of a Kind' }
+local function pick_random_hand()
+  local hands = {}
+  for k in pairs(G.GAME.hands) do
+    table.insert(hands, k)
+  end
+  return hands[pseudorandom('joker_metro_hand', 1, #hands)]
+end
 
 SMODS.Joker {
   key = 'joker_metro',
@@ -7,7 +13,7 @@ SMODS.Joker {
   blueprint_compat = true,
   perishable_compat = false,
   rental_compat = true,
-  config = { extra = { mult = 0, gain = 2, gain_increase = 2, current_hand_index = 1 } },
+  config = { extra = { mult = 0, gain = 2, gain_increase = 2, current_hand = 'High Card' } },
   unlocked = true,
   discovered = false,
   rarity = 2,
@@ -15,26 +21,28 @@ SMODS.Joker {
   pos = { x = 0, y = 5 },
   cost = 7,
   loc_vars = function(self, info_queue, card)
-    local current_hand = hand_order[card.ability.extra.current_hand_index] or 'High Card'
-    return { vars = { card.ability.extra.mult, card.ability.extra.gain, localize(current_hand, 'poker_hands'), card.ability.extra.gain_increase } }
+    return {
+      vars = {
+        card.ability.extra.mult,
+        card.ability.extra.gain,
+        localize(card.ability.extra.current_hand, 'poker_hands'),
+        card.ability.extra.gain_increase,
+      },
+    }
   end,
   calculate = function(self, card, context)
-    local current_hand = hand_order[card.ability.extra.current_hand_index] or 'High Card'
-
-    if context.setting_blind and not context.blueprint then
-      card.ability.extra.current_hand_index = pseudorandom('joker_metro_hand', 1, #hand_order)
+    if context.end_of_round and context.main_eval and not context.blueprint then
+      card.ability.extra.current_hand = pick_random_hand()
     end
 
-    if context.blind_defeated and not context.blueprint then
+    if context.blind_defeated and not context.blueprint and G.GAME.blind.boss then
       card.ability.extra.gain = card.ability.extra.gain + card.ability.extra.gain_increase
       return { message = localize('k_upgrade_ex'), colour = G.C.MULT }
     end
 
-    if context.before and not context.blueprint then
-      if context.scoring_name == current_hand then
-        card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.gain
-        return { message = localize('k_upgrade_ex'), colour = G.C.MULT }
-      end
+    if context.before and not context.blueprint and context.scoring_name == card.ability.extra.current_hand then
+      card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.gain
+      return { message = localize('k_upgrade_ex'), colour = G.C.MULT }
     end
 
     if context.joker_main and card.ability.extra.mult > 0 then
