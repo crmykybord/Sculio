10a21c46f74824dade0d58a50017e6934f6e8796
Marffe <efframlol@gmail.com>
2026-07-14 23:07:07 -0500

    Joker of Nothing working as intended

--- jokers/59_joker_of_nothing.lua
+++ jokers/59_joker_of_nothing.lua
@@ -1,3 +1,12 @@
+local function count_missing_ranks()
+  if not G.playing_cards then return 13 end
+  local existing = {}
+  for _, c in ipairs(G.playing_cards) do existing[c:get_id()] = true end
+  local missing = 0
+  for i = 2, 14 do if not existing[i] then missing = missing + 1 end end
+  return missing
+end
+
 SMODS.Joker {
   key = 'joker_of_nothing',
   attributes = { 'xmult', 'king' },
@@ -5,7 +14,7 @@ SMODS.Joker {
   blueprint_compat = true,
   perishable_compat = true,
   rental_compat = true,
-  config = { extra = { x_mult_per_rank = 0.2 } },
+  config = { extra = { x_mult_per_rank = 0.25 } },
   unlocked = true,
   discovered = false,
   rarity = 3,
@@ -13,45 +22,19 @@ SMODS.Joker {
   pos = { x = 1, y = 6 },
   cost = 8,
   loc_vars = function(self, info_queue, card)
-    local missing_ranks = 0
-    local existing_ranks = {}
-    if G.playing_cards then
-      for _, c in ipairs(G.playing_cards) do
-        local rank = c:get_id()
-        existing_ranks[rank] = true
-      end
-      for i = 2, 14 do
-        if not existing_ranks[i] then
-          missing_ranks = missing_ranks + 1
-        end
-      end
-    else
-      missing_ranks = 13
-    end
-    local total_xmult = 1 + (card.ability.extra.x_mult_per_rank * missing_ranks)
-    return { vars = { card.ability.extra.x_mult_per_rank, missing_ranks, total_xmult } }
+    local missing = count_missing_ranks()
+    return { vars = { card.ability.extra.x_mult_per_rank, missing, 1 + card.ability.extra.x_mult_per_rank * missing } }
   end,
   calculate = function(self, card, context)
-    if context.individual and context.cardarea == G.play then
-      if context.other_card:get_id() == 13 then
-        local missing_ranks = 0
-        local existing_ranks = {}
-        if G.playing_cards then
-          for _, c in ipairs(G.playing_cards) do
-            local rank = c:get_id()
-            existing_ranks[rank] = true
-          end
-          for i = 2, 14 do
-            if not existing_ranks[i] then
-              missing_ranks = missing_ranks + 1
-            end
-          end
-        else
-          missing_ranks = 13
-        end
-        local total_xmult = 1 + (card.ability.extra.x_mult_per_rank * missing_ranks)
-        return { xmult = total_xmult, card = card }
+    if context.individual and context.cardarea == G.play and context.other_card:get_id() == 13 then
+      local missing = count_missing_ranks()
+      if card.ability.extra.last_missing == nil then
+        card.ability.extra.last_missing = missing
+      elseif missing > card.ability.extra.last_missing then
+        card.ability.extra.last_missing = missing
+        return { xmult = 1 + card.ability.extra.x_mult_per_rank * missing, card = card, message = localize('k_upgrade_ex'), message_card = card }
       end
+      return { xmult = 1 + card.ability.extra.x_mult_per_rank * missing, card = card }
     end
   end
 }
