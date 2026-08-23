a0acb209d023f1b681f458475b444d37867ea63f
Marffe <efframlol@gmail.com>
2026-08-23 13:03:19 -0500

    Game Package now works more like Baron, also nerfed

--- jokers/60_game_package.lua
+++ jokers/60_game_package.lua
@@ -5,13 +5,13 @@ SMODS.Joker {
   blueprint_compat = true,
   perishable_compat = true,
   rental_compat = true,
-  config = { x_mult = 1, extra = { x_mult = 0 } },
+  config = { x_mult = 0.5, extra = { x_mult = 0 } },
   unlocked = true,
   discovered = false,
-  rarity = 2,
+  rarity = 3,
   atlas = 'Sculio',
   pos = { x = 2, y = 6 },
-  cost = 5,
+  cost = 7,
   loc_vars = function(self, info_queue, card)
     return { vars = { card.ability.x_mult } }
   end,
@@ -22,10 +22,7 @@ SMODS.Joker {
     if context.end_of_round then return nil end
     if context.individual and context.cardarea == G.hand then
       if context.other_card.base.id == 2 or context.other_card.base.id == 4 then
-        return {
-          xmult = card.ability.x_mult * card.ability.extra.x_mult,
-          card = context.other_card,
-        }
+        return { xmult = card.ability.x_mult * card.ability.extra.x_mult, card = context.other_card, }
       end
     end
   end
