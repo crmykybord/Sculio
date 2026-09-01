0016c65b946ac3a0d9ca45dd5bddc02136a99302
Marffe <efframlol@gmail.com>
2026-08-31 22:27:04 -0500

    Sticky Keys buff

--- jokers/21_sticky_keys.lua
+++ jokers/21_sticky_keys.lua
@@ -1,14 +1,13 @@
 SMODS.Joker {
   key = 'sticky_keys',
   attributes = { 'mult', 'rank' },
-
   config = { extra = { mult = 0, hands_until_change = 3, hands_elapsed = 0 } },
   unlocked = true,
   discovered = false,
   rarity = 1, -- Common
   atlas = 'Sculio',
   pos = { x = 2, y = 2 },
-  cost = 3,
+  cost = 2,
   eternal_compat = true,
   blueprint_compat = true,
   perishable_compat = true,
@@ -18,10 +17,7 @@ SMODS.Joker {
   end,
   calculate = function(self, card, context)
     if context.joker_main and card.ability.extra.mult > 0 then
-      return {
-        mult = card.ability.extra.mult,
-        message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
-      }
+      return { mult = card.ability.extra.mult }
     end
 
     if context.after and not context.blueprint then
@@ -37,10 +33,8 @@ SMODS.Joker {
             base_chips = 10
           end
         end
-
         card.ability.extra.mult = base_chips
         card.ability.extra.hands_elapsed = 0
-
         return { message = localize('k_Sculio_sticky_keys_changed') }
       end
     end
