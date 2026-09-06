ae46b1672bb07983dbbdeb38f911e706e8ab251c
Marffe <efframlol@gmail.com>
2026-09-06 16:04:16 -0500

    Dynamic Description for Puck and Figurine

--- jokers/100_puck.lua
+++ jokers/100_puck.lua
@@ -1,3 +1,22 @@
+-- Full 4-stat text used when an XChips edition exists (e.g. Bunco)
+Sculio.PUCK_ALT_TEXT = {
+  ['en-us'] = {
+    'When a card with an {C:attention}edition{} is scored,',
+    'this Joker gains the {C:attention}edition\'s bonus{}',
+    '{C:inactive,s:0.8}(Currently {C:chips,s:0.8}+#1#{}{C:inactive,s:0.8} Chips, {C:mult,s:0.8}+#2#{}{C:inactive,s:0.8} Mult, {X:mult,C:white,s:0.8}X#3#{}{C:inactive,s:0.8} Mult, and {X:chips,C:white,s:0.8}X#4#{}{C:inactive,s:0.8} Chips)',
+  },
+  ['es_419'] = {
+    'Anotar cartas con {C:dark_edition}edición{} otorga',
+    'sus {C:attention}bonificaciones{} a este comodín',
+    '{C:inactive,s:0.8}(Actualmente {C:chips,s:0.8}+#1#{}{C:inactive,s:0.8} Fichas, {C:mult,s:0.8}+#2#{}{C:inactive,s:0.8} Multi, {X:mult,C:white,s:0.8}X#3#{}{C:inactive,s:0.8} Multi y {X:chips,C:white,s:0.8}X#4#{}{C:inactive,s:0.8} Fichas)',
+  },
+  ['es_ES'] = {
+    'Anotar cartas con {C:dark_edition}edición{} otorga',
+    'sus {C:attention}bonificaciones{} a este comodín',
+    '{C:inactive,s:0.8}(Actualmente {C:chips,s:0.8}+#1#{}{C:inactive,s:0.8} Fichas, {C:mult,s:0.8}+#2#{}{C:inactive,s:0.8} Multi, {X:mult,C:white,s:0.8}X#3#{}{C:inactive,s:0.8} Multi y {X:chips,C:white,s:0.8}X#4#{}{C:inactive,s:0.8} Fichas)',
+  },
+}
+
 SMODS.Joker {
   key = 'puck',
   attributes = { 'chips', 'mult', 'xmult', 'xchips', 'editions', "scaling" },
@@ -14,7 +33,7 @@ SMODS.Joker {
   soul_pos = { x = 9, y = 1 },
   cost = 20,
   loc_vars = function(self, info_queue, card)
-    return { vars = { card.ability.extra.chips, card.ability.extra.mult, card.ability.extra.x_mult, card.ability.extra.x_chips, card.ability.extra.bonus_mult * 100 } }
+    return { vars = { card.ability.extra.chips, card.ability.extra.mult, card.ability.extra.x_mult, card.ability.extra.x_chips } }
   end,
   calculate = function(self, card, context)
     if context.individual and context.cardarea == G.play and not context.blueprint then
