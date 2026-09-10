3d704e64b48086d6a2e3dede900c8967ecc22119
Marffe <efframlol@gmail.com>
2026-09-10 00:01:53 -0500

    Lead removed from Enhecements pool to prevents akward situations

--- enhancements/lead.lua
+++ enhancements/lead.lua
@@ -7,6 +7,9 @@ SMODS.Enhancement {
   loc_vars = function(self, info_queue, card)
     return { vars = {} }
   end,
+  in_pool = function(self)
+    return false
+  end,
   -- Always placed at the bottom of the deck: handled by the deck shuffle
   -- hook in libs/shuffle.lua (same mechanism as Verified User).
 }
