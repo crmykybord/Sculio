d423baf4823db35d66aad137f991da7cb7cf63f4
Marffe <efframlol@gmail.com>
2026-09-09 23:07:53 -0500

    Buff Soup

--- jokers/38_soup.lua
+++ jokers/38_soup.lua
@@ -5,7 +5,7 @@ SMODS.Joker {
   blueprint_compat = true,
   perishable_compat = true,
   rental_compat = true,
-  config = { extra = { x_mult = 1.0, x_mult_gain = 0.03, x_mult_max = 2 } },
+  config = { extra = { x_mult = 1.0, x_mult_gain = 0.04, x_mult_max = 2 } },
   unlocked = true,
   discovered = false,
   rarity = 1, -- Common
