2bf484fed47ffa47ce978d97124ce871fa2baf11
Marffe <efframlol@gmail.com>
2026-09-17 00:36:47 -0500

    I hope im really not this stupid

--- seals/inverted.lua
+++ seals/inverted.lua
@@ -5,21 +5,28 @@ SMODS.Seal {
   badge_colour = HEX 'B14AB8',
   config = {},
   calculate = function(self, card, context)
-    -- Mirror of the vanilla Purple Seal, but creates an Inverted Tarot
-    if context.discard and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
-      G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
-      G.E_MANAGER:add_event(Event({
-        trigger = 'before',
-        delay = 0.0,
-        func = (function()
-          local new_card = create_card('Inverted', G.consumeables, nil, nil, nil, nil, nil, 'sculio_inv_seal')
-          new_card:add_to_deck()
-          G.consumeables:emplace(new_card)
-          G.GAME.consumeable_buffer = 0
-          return true
-        end)
-      }))
-      card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize('k_Sculio_plus_inverted'), colour = G.C.SET.Inverted })
+    -- Mirror of the vanilla Purple Seal, but for Inverted Tarots (a single one)
+    if context.discard and context.other_card == card then
+      local room = G.consumeables.config.card_limit - (#G.consumeables.cards + G.GAME.consumeable_buffer)
+      local copies = math.min(1, room)
+      if copies > 0 then
+        G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + copies
+        G.E_MANAGER:add_event(Event({
+          trigger = 'before',
+          delay = 0.0,
+          func = (function()
+            for i = 1, copies do
+              local new_card = create_card('Inverted', G.consumeables, nil, nil, nil, nil, nil, 'sculio_inv_seal' .. i)
+              new_card:add_to_deck()
+              G.consumeables:emplace(new_card)
+            end
+            G.GAME.consumeable_buffer = 0
+            return true
+          end)
+        }))
+        local msg = copies == 1 and localize('k_Sculio_plus_inverted') or ('+' .. copies .. ' ' .. localize('b_inverted_cards'))
+        card_eval_status_text(card, 'extra', nil, nil, nil, { message = msg, colour = G.C.SET.Inverted })
+      end
     end
   end,
 }
