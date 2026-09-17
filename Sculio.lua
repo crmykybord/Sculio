e75ab3bc735fac8ae4966ccc0f8a774dd94ba27e
Marffe <efframlol@gmail.com>
2026-09-17 00:44:06 -0500

    Removing shop rate thing

--- Sculio.lua
+++ Sculio.lua
@@ -5,7 +5,7 @@ SMODS.Atlas { key = 'Sculio_Consumables', path = 'Consumables.png', px = 71, py 
 SMODS.Atlas { key = 'Sculio_Enhancements', path = 'Enhancements.png', px = 71, py = 95 }
 SMODS.Atlas { key = 'Sculio_Booster', path = 'Booster Packs.png', px = 71, py = 95 }
 SMODS.Atlas { key = 'Sculio_Vouchers', path = 'Vouchers.png', px = 71, py = 95 }
-SMODS.ConsumableType { key = 'Inverted', primary_colour = HEX 'B14AB8', secondary_colour = HEX 'A84C45', collection_rows = { 6, 5 }, shop_rate = 4 }
+SMODS.ConsumableType { key = 'Inverted', primary_colour = HEX 'B14AB8', secondary_colour = HEX 'A84C45', collection_rows = { 6, 5 } }
 
 SMODS.current_mod.optional_features = function()
   return { post_trigger = true }
