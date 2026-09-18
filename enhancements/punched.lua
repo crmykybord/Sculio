6eba18ff4b43458da96dd9df5be53f1204faa26c
Marffe <efframlol@gmail.com>
2026-09-17 22:53:38 -0500

    removing hardcoded value

--- enhancements/punched.lua
+++ enhancements/punched.lua
@@ -3,16 +3,16 @@ SMODS.Enhancement {
   atlas = 'Sculio_Enhancements',
   pos = { x = 0, y = 1 },
 
-  config = {},
+  config = { extra = { x_mult = 1.5 } },
   loc_vars = function(self, info_queue, card)
-    return { vars = { 1.5 } }
+    return { vars = { card.ability.extra.x_mult } }
   end,
   calculate = function(self, card, context)
     if context.discard and context.other_card == card then
       return { remove = true }
     end
     if context.main_scoring and context.cardarea == G.play then
-      return { x_mult = 1.5, }
+      return { x_mult = card.ability.extra.x_mult, }
     end
   end,
 }
