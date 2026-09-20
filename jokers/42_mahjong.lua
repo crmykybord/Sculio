385bfaccffa7ea2c559313cc80a2566f887d4e82
Marffe <efframlol@gmail.com>
2026-09-20 09:15:21 -0500

    Minor code fixes and migration

--- jokers/42_mahjong.lua
+++ jokers/42_mahjong.lua
@@ -52,7 +52,7 @@ SMODS.Joker {
     end
 
     if context.joker_main and card.ability.extra.chips > 0 then
-      return { chips = card.ability.extra.chips, message = localize{type='variable',key='a_chips',vars={card.ability.extra.chips}} }
+      return { chips = card.ability.extra.chips }
     end
   end
 }
