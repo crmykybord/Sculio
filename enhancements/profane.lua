33531c85317ee7587adffd12fac8d4535722ba9d
Marffe <efframlol@gmail.com>
2026-09-23 07:25:59 -0500

    Wrong scaling storage

--- enhancements/profane.lua
+++ enhancements/profane.lua
@@ -53,7 +53,7 @@ SMODS.Enhancement {
           end
         end
       end
-      card.ability.bonus = (card.ability.bonus or 0) + gain
+      card.ability.perma_bonus = (card.ability.perma_bonus or 0) + gain
       return {
         chips = gain,
         message = localize { type = 'variable', key = 'a_chips', vars = { gain } },
